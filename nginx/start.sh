087aad5376732d386e61777efd40c53efa62a33d
volutus <trevcat3@hotmail.com>
2024-10-08 15:36:54 -0400

    Try injecting env variable by copying start.sh logic

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
-envsubst '$UPTIME_KUMA_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+# envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+# envsubst '$UPTIME_KUMA_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'

e905342a3b6b5bdfbb119317ec011d144a2f7ac2
volutus <trevcat3@hotmail.com>
2024-10-08 15:37:56 -0400

    Remove unused files?

--- nginx/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
-# envsubst '$UPTIME_KUMA_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'

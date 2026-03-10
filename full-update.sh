a56f64e89ff9b9c5dafa08c30593e5d118b9861a
volutus <trevcat3@hotmail.com>
2026-03-09 21:33:49 -0400

    Updater script to always pull latest (hard force)

--- full-update.sh
+++ full-update.sh
@@ -1,4 +1,4 @@
 pushd /home/flask/self-domain-docker
 git pull origin master
-docker compose up -d --force-recreate --no-deps --build
+docker compose up -d --force-recreate --no-deps --build --pull always
 popd
